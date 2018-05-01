db875eedeb357b4473e33a629aeb18186e93a34b
aaron.zeng <aaron.zeng@wtmec.com>
2018-04-30 19:04:06 -0700

    add file install_package.sh

---
+++ 1.INSTALL_FOR_UBUNTU/6.install_package.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+rc=1
+while [ "$rc" != 0 ]; do
+    sudo apt-get install gawk wget git git-core diffstat unzip texinfo gcc-multilib build-essential chrpath socat libsdl1.2-dev lib32stdc++6 lib32z1 libsdl1.2-dev xterm sed cvs subversion coreutils texi2html docbook-utils python-pysqlite2 help2man make gcc g++ desktop-file-utils libgl1-mesa-dev libglu1-mesa-dev mercurial autoconf automake groff curl lzop asciidoc u-boot-tools python python3 python3-pip python3-pexpect python-git xz-utils debianutils iputils-ping lib32stdc++6 lib32z1 git-core gnupg flex bison gperf build-essential zip gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev ccache libgl1-mesa-dev libxml2-utils xsltproc unzip uuid uuid-dev zlib1g-dev liblz-dev liblzo2-2 liblzo2-dev lzop git-core curl u-boot-tools mtd-utils android-tools-fsutils uuid uuid-dev zlib1g-dev liblz-dev android-tools-fsutils device-tree-compiler
+
+rc=$?
+done
+echo "finish!!!!!!!!!!!!!"
